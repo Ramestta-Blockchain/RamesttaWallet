78b6259fe36793dce13f82bfc1b93f188ea724c0
sharique-ramestta <sharique.it@ramaera.in>
2023-11-03 17:59:01 +0530

    initial commit

---
+++ app/src/test/java/com/alphawallet/app/util/DappBrowserUtilsTest.java
@@ -0,0 +1,36 @@
+package com.alphawallet.app.util;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DappBrowserUtilsTest
+{
+    @Test
+    public void testDefaultDapp()
+    {
+        assertThat(DappBrowserUtils.defaultDapp(1L), equalTo("https://ramestta.com/"));
+        assertThat(DappBrowserUtils.defaultDapp(137L), equalTo("https://ramestta.com/"));
+        assertThat(DappBrowserUtils.defaultDapp(80001L), equalTo("https://ramestta.com/"));
+    }
+
+    @Test
+    public void testIsDefaultDapp()
+    {
+        assertTrue(DappBrowserUtils.isDefaultDapp("https://ramestta.com/"));
+        assertTrue(DappBrowserUtils.isDefaultDapp("https://ramestta.com/"));
+        assertFalse(DappBrowserUtils.isDefaultDapp("https://app.1inch.finance/"));
+    }
+
+    @Test
+    public void testIsWithinHomePage()
+    {
+        assertTrue(DappBrowserUtils.isWithinHomePage("https://ramestta.com/"));
+        assertTrue(DappBrowserUtils.isWithinHomePage("https://ramestta.com/"));
+        assertTrue(DappBrowserUtils.isWithinHomePage("https://ramestta.com/"));
+        assertFalse(DappBrowserUtils.isWithinHomePage("https://ramestta.com/"));
+    }
+}
